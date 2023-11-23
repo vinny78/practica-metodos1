79bd04c5860d7a829f6e11987b481bfad4ca9dcb
vinny78 <146063659+vinny78@users.noreply.github.com>
2023-11-23 12:03:58 +0100

    Add files via upload

--- src/prueba9.java
+++ src/prueba9.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class prueba9 {
+    public static void main(String[] args) {
+        imprimirTriangulo('a', 4);
+        Scanner in = new Scanner(System.in);
+    }
+
+    static void imprimirTriangulo(char caracter, int numLineas) {
+        for (int i = 1; i <= numLineas; i++) {
+            // Imprimir espacios en blanco
+            for (int j = 0; j < numLineas - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Imprimir caracteres
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print(caracter);
+            }
+
+            // Cambiar de línea después de cada fila
+            System.out.println();
+        }
+    }
 }
