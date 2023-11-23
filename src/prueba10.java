1b9322d2087196a08899b058f3186c1a804ac19e
vinny78 <146063659+vinny78@users.noreply.github.com>
2023-11-23 12:20:48 +0100

    Add files via upload

---
+++ src/prueba10.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class prueba10 {
+    public static void showMenu(){
+        System.out.println(" (A) Muestra el signo de un número introducido por el usuario");
+        System.out.println(" (B) indica so el usuario es mayor de edad o no ");
+        System.out.println(" (C) calcula el área y perimetro de un circulo");
+        System.out.println(" (D) Conversor de euros a dolares y de dolares a euros ");
+        System.out.println(" (E) Mostrar tabla de multiplicar de un numero");
+        System.out.println(" (F) Mostrar tablas de multiplicar del 1 al 10");
+        System.out.println(" (G) Comprobador de números primos");
+        System.out.println(" (H) comprobador de fechas");
+        System.out.println(" (I) Dibujar triangulos");
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        showMenu();
+
+
+
+    }
+}
