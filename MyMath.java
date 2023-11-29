97b9548512d67eeb9968ce8a6c40c13c26d6c72f
vinny78 <146063659+vinny78@users.noreply.github.com>
2023-11-29 13:57:39 +0100

    Add files via upload

---
+++ MyMath.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+public class MyMath {
+    public static double squarePerimeter (double perimetro){
+        double valorLado = 0;
+        double PerimetroCuadrado = valorLado*4;
+        return perimetro;
+    }
+    public static double squareArea (double area){
+        double valorLado = 0;
+        double PerimetroCuadrado = valorLado*valorLado;
+        return area;
+    }
+    public static double  rectanglePerimeter (double perimetro){
+        double valorLado = 0;
+        double valorAltura = 0;
+        double PerimetroRectangulo = valorLado + valorAltura*2;
+        return perimetro;
+    }
+    public static double rectangleArea (double area){
+        double valorLargo = 0;
+        double valorAncho = 0;
+        double Perimetrorectangulo = valorLargo*valorAncho;
+        return area;
+    }
+    public static double  circlePerimeter (double perimetro){
+        double valorPI = 3.14;
+        double valorDiametro = 0;
+        double PerimetroCirculo = valorPI*valorDiametro;
+        return perimetro;
+    }
+    public static double  circleArea (double area){
+        double valorPI = 3.14;
+        double valorRadio = 0;
+        double areaCirculo = valorPI*Math.pow(valorRadio,2);
+        return area;
+    }
+
+
+
+
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println();
+
+        }
+
+    }
